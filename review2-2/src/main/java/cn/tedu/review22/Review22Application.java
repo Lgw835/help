70cb044c276c66316863c4fa8d934ff70d2b9b6e
Maxwell_li <3158918082@qq.com>
2024-11-14 22:12:53 +0800

    JDBC stu

---
+++ review2-2/src/main/java/cn/tedu/review22/Review22Application.java
@@ -0,0 +1,13 @@
+package cn.tedu.review22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Review22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Review22Application.class, args);
+    }
+
+}
---
+++ review2-2/src/test/java/cn/tedu/review22/Review22ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.review22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Review22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
